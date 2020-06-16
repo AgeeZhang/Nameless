0193ac4ebf6924d81bb1510180c2284ede85c790
zzj <zzj@qq.com>
2020-06-16 17:36:32 +0800

    init

---
+++ src/test/java/com/banxian/nameless/NamelessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banxian.nameless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamelessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
